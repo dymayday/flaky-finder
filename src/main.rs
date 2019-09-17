eb22ce7a83fbb9098519f136e1c4b7266e4002ad
dymayday <dymayday@gmail.com>
2019-09-17 16:43:14 -0400

    Add parallel capability

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![feature(try_trait)]
 use error::{FlakyFinderResult};
 use builder::FlakyFinderBuilder;
-use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 use std::{
     io::Write,
     process::{Command, ExitStatus},
+    thread,
 };
+use crossbeam_channel::bounded;
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 
 mod error;
 mod builder;
@@ -27,7 +29,7 @@ pub(crate) struct FlakyFinder {
 
 impl FlakyFinder {
     /// Runs a command multiple time trying to find if it can fail at some point.
-    pub(crate) fn run(&mut self) -> FlakyFinderResult<()> {
+    pub(crate) fn run(&self) -> FlakyFinderResult<()> {
         // println!("param = {:#?}", self);
 
         // Provide a custom bar style
@@ -56,11 +58,75 @@ impl FlakyFinder {
         }
         Ok(())
     }
+
+    /// Runs a command multiple time trying to find if it can fail at some point.
+    pub(crate) fn par_run(cmd: &str, runs: u64) -> FlakyFinderResult<()> {
+
+        // Provide a custom bar style
+        let pb = ProgressBar::new(runs);
+        pb.set_style(ProgressStyle::default_bar().template(
+            "{spinner:.cyan} [{elapsed_precise}] [{bar:40.white/gray}] ({pos}/{len}, ETA {eta})",
+        ));
+
+
+        let (sx, rx) = bounded(runs as usize);
+
+        let cmd = std::sync::Arc::new(cmd.to_string());
+
+        let output = Command::new("sh")
+            .arg("-c")
+            // .arg(cmd.clone().to_string())
+            .arg(cmd.to_string())
+            .output()
+            .expect("Fail to run command process.");
+
+        sx.send(output).expect("Fail to send Command's output to channel.");
+
+
+        // TODO: Run the tests in parallel
+        // for _ in (0..self.runs).progress_with(pb) {
+        for _ in (0..runs-1) {
+            // std::thread::sleep(std::time::Duration::from_millis(100));
+
+            let cmd = cmd.clone();
+            let sx = sx.clone();
+
+            thread::spawn(move || {
+                let output = Command::new("sh")
+                    .arg("-c")
+                    // .arg(cmd.clone().to_string())
+                    .arg(cmd.to_string())
+                    .output()
+                    .expect("Fail to run command process.");
+
+                sx.send(output).expect("Fail to send Command's output to channel.");
+            });
+        }
+
+        drop(sx);
+
+        for recv_output in rx.iter().progress_with(pb) {
+            let status = recv_output.status;
+            if !status.success() {
+                std::io::stdout().write_all(&recv_output.stdout)?;
+                std::io::stderr().write_all(&recv_output.stderr)?;
+                break;
+            }
+        }
+        drop(rx);
+
+        Ok(())
+    }
 }
 
 fn main() {
-    let mut ff = FlakyFinderBuilder::from_cli().build();
-    ff.run().expect("Fail to spawn.");
+    let ff = FlakyFinderBuilder::from_cli().build();
+
+    if ff.nb_threads > 1 {
+        FlakyFinder::par_run(&ff.cmd, ff.runs).expect("Fail to run processes in parallel.");
+    } else {
+        ff.run().expect("Fail to processes.");
+    }
 }
 
 #[cfg(test)]
