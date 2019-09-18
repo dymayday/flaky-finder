b8bb0f34ebb4e9eb9fea1e430da9c86638cd9d2a
dymayday <dymayday@gmail.com>
2019-09-18 10:33:07 -0400

    Add utils

---
+++ src/utils.rs
@@ -0,0 +1,20 @@
+//! Parking several utility stuff here.
+
+use crate::error::FlakyFinderResult;
+use std::io::{stderr, stdout, Write};
+
+
+/// Print a vector of byte as String and flush the stdout buffer.
+pub fn fstdout(output: &[u8]) -> FlakyFinderResult<()> {
+    stdout().write_all(output)?;
+    stdout().flush()?;
+    Ok(())
+}
+
+
+/// Print a vector of byte as String and flush the stderr buffer.
+pub fn fstderr(output: &[u8]) -> FlakyFinderResult<()> {
+    stderr().write_all(output)?;
+    stderr().flush()?;
+    Ok(())
+}
